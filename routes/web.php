3ba4f1e0c4a88c2eadc41e8469e568e6360c4a15
Fabian0121 <fabianjuarezsandoval0121@gmail.com>
2021-11-25 16:10:16 -0600

    rutas

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CarritoController;
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
