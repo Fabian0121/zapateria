c20eea98c3b3f4e32af1fc2ef7ab71e6546a653d
Fabian0121 <fabianjuarezsandoval0121@gmail.com>
2021-11-25 16:04:51 -0600

    rutas

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,20 @@ Route::get('/', function () {
 
 Route::get('/',[AdminController::class,'agregarCalzado'])->name('agregar');
 Route::post('/agregar',[AdminController::class, 'agregar']) -> name('agregar.form');
+//Rutas para vista login y registro usuarios
+//login
+Route::get("/registro", [UserController::class, 'viewRegistro'])->name('registroView');
+Route::post('/registro/function', [UserController::class, 'registro'])->name('registro');
+//registro
+Route::get('/login', [UserController::class, 'loginView'])->name('loginView');
+Route::post('/login/function', [UserController::class, 'login'])->name('login');
+//logout
+Route::get('/logout', [UserController::class, 'logout'])->name('logout');
+//Usuarios
+Route::prefix('/user')->middleware('verificarUsuario')->group(function(){
+    //Datos de usuario
+    Route::get("/inicio",[UserController::class,'inicio'])->name('user.inicio'); 
+
+    //Carrito
+    Route::get("/carrito",[CarritoController::class,'verCarrito'])->name('carrito'); 
+});
